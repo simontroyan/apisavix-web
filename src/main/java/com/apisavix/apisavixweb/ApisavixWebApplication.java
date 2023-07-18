f1c7dbbd177aa71481565dcff036d084f70800e7
simontroyan <troyan.simon@gmail.com>
2023-07-19 01:28:48 +0200

    Initial commit

---
+++ src/main/java/com/apisavix/apisavixweb/ApisavixWebApplication.java
@@ -0,0 +1,13 @@
+package com.apisavix.apisavixweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisavixWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApisavixWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apisavix/apisavixweb/ApisavixWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apisavix.apisavixweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisavixWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
